7e8b95ac472b1f520a761f4acce792831ef24736
kelvin <kelvinjps@proton.me>
2025-06-11 21:05:02 -0400

    implement optimizations to countdown

--- countdown.js
+++ countdown.js
@@ -17,22 +17,22 @@ function convertMillisecondsToTime(milliseconds) {
     seconds: remainingSeconds
   };
 }
-function countdown(element, targetDate){
-    const currentDate = new Date();
+function countdown(element, targetTimestamp){
+    const currentTimestamp = Date.now();
     // substract dates this is done in miliseconds
-    const diffDateMiliseconds = targetDate - currentDate;
+    const diffDateMiliseconds = targetTimestamp - currentTimestamp;
     // convert it to days hours minutes and seconds and display in the countdowntext
     const diffDate = convertMillisecondsToTime(diffDateMiliseconds);
     element.textContent = `Faltan ${diffDate.days}d:${diffDate.hours}h:${diffDate.minutes}m:${diffDate.seconds}s`
 }
 
-window.addEventListener("load", (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
   const countdown_text = document.getElementById("countdown_text")
   if (countdown_text != null) {
     const XVDATE= new Date("2025-08-16T18:00:00-05:00"); //August 18 at 18:00 pm in Colombia in ISO format
-    const originalDate = new Date(); // Get the current date and time
+    const targetTimestamp = XVDATE.getTime(); // Calculate timestamp once
 
-    const intervalId = setInterval(countdown,1000,countdown_text,XVDATE) // update countdown text every second
+    const intervalId = setInterval(countdown,1000,countdown_text,targetTimestamp) // update countdown text every second
     setTimeout(function() {
       clearInterval(intervalId)
     }, XVDATE -  new Date());
